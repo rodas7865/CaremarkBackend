ba4361ab8c97d980836fbe428b629d1985f19277
rodas7865 <64026265+rodas7865@users.noreply.github.com>
2022-02-02 13:35:25 +0000

    update escala

--- models/escala.js
+++ models/escala.js
@@ -8,16 +8,16 @@ const escalaSchema = new Schema(
             required:[true, 'É necessario confirmação para o documento']
         },
         user_id : {
-            type:String,
+            type:[String],
             required:[true, 'É necessario id do utilizador']
         },
-        hora : {
-            type:[Number],
-            validate:[horaValid,'Hora não é valida (não introduzio hora, numeros não são validos ou não seguio a ordem do menor para maior)']
+        inicio : {
+            type:Date,
+            required:[true, 'É necessario hora de inicio']
         },
-        date:{
-            type: [Number],
-            validate:[dateValid,'Date não é valida (formato : dd/mm/yyyy, não pode inserir mais / menos que 3 valores)']
+        fim:{
+            type: Date,
+            required:[true, 'É necessario hora de fim']
         },
         notas : {
             type:String,
@@ -32,38 +32,3 @@ const escalaSchema = new Schema(
 const  modelEscala = mongoose.model('Escala',escalaSchema);
 
 module.exports = modelEscala
-
-function dateValid(val){
-    let valid = false
-    if(val.length===3){
-     if(val[0]>31){
-         return valid //false
-     }
-     if(val[1]>12){
-         return valid //false
-     }
-     if(val[2]<1990){
-         return valid //false
-     }
-     valid = true
-    }
-    return valid
-}
-
-function  horaValid(val){
-    let valid = false
-
-        if(val.length===2){
-
-            if(val[0]>24||val[0]===0||val[1]>24||val[1]===0){
-                return valid
-            }
-
-            if(val[1]<val[0]){
-                return valid
-            }
-            valid = true
-        }
-
-    return valid
-}
