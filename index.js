463c6c82450420a99fb641bf923bb2eb10affa92
rodas7865 <64026265+rodas7865@users.noreply.github.com>
2022-02-03 12:27:10 +0000

    update cors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express"),
-    app = express(),
-    cors = require('cors'),
-    port = process.env.PORT || 8080,
-    projName = "Caremark",
-    url ="mongodb://admin:admin@tdwficha-shard-00-00.swz9o.mongodb.net:27017,tdwficha-shard-00-01.swz9o.mongodb.net:27017,tdwficha-shard-00-02.swz9o.mongodb.net:27017/Caremark?ssl=true&replicaSet=atlas-42jsfi-shard-0&authSource=admin&retryWrites=true&w=majority",
-    dbName = "Caremark",
-    mongoose = require("mongoose"),
-    connect =mongoose.connect(url,{dbName:dbName, useNewUrlParser: true, useUnifiedTopology: true})
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 8080;
+const projName = "Caremark";
+const url ="mongodb://admin:admin@tdwficha-shard-00-00.swz9o.mongodb.net:27017,tdwficha-shard-00-01.swz9o.mongodb.net:27017,tdwficha-shard-00-02.swz9o.mongodb.net:27017/Caremark?ssl=true&replicaSet=atlas-42jsfi-shard-0&authSource=admin&retryWrites=true&w=majority";
+const dbName = "Caremark";
+const mongoose = require("mongoose");
+const connect =mongoose.connect(url,{dbName:dbName, useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(function (req,res,next){
     let date = new Date(),
         dataEtempo = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" em "+date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
